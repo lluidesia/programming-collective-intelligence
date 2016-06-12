859eefc782f4b7149f15f657883cbbb4d565e47e
lluidesia <liudaprysiazhna@gmail.com>
2016-06-12 22:07:24 +0300

    'recommendations'

---
+++ recommendations.py
@@ -0,0 +1,21 @@
+#Збір інформації про уподобання
+
+#перше, що потрібно - це представити людей і їхні вподобання. 
+#зробимо це задопомогою вкладеного словника
+
+#словник кінокритиків і виставлених ними оцінок для невеликого набору даних про фільми
+critics={'Lisa Rose':{'Lady in the Water':2.5,'Snakes on a Plane':3.5,'Just My Luck':3.0,
+						'Super Returns':3.5,'You, Me and Dupree':2.5,'The Night Listener':3.0},
+			'Gene Seymour':{'Lsdy in Water':3.0,'Snakes on a Plane':3.5,'Just My Luck':1.5,
+							'Superman Returns':5.0,'The Night Listener':3.0,'You, Me and Dupree':3.5},
+			'Michael Phillips':{'Lady in the Water':2.5,'Snakes on a Plane':3.0,'Super Returns':3.5,'The Night Listener':4.0},
+			'Claudia Puig':{'Snakes on a Plane':3.5,'Just My Luck':3.0,'The Night Listener':4.5,'Superman Returns':4.0,'You, Me and Dupree':2.5},
+			'Nick LaSalle':{'Lady in the Water':3.0,'Snakes on a Plane':4.0,'Just My Luck':2.0,'Superman Returns':3.0,
+							'The Night Listener':3.0,'You, Me and Dupree':2.0},
+			'Jack Matthew':{'Lady in the Water':3.0,'Snakes on a Plane':4.0,'The Night Listener':3.0,'Superman Returns':5.0,'You, Me and Dupree':3.5},
+			'Toby':{'Snakes on a Plane':4.5,'You, Me and Dupree':1.0,'Superman Returns':4.0}
+						}
+
+# from recommendatins import critics
+# critics['Lisa Rose']['Lady in the Water']
+# 2.5
